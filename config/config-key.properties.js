f2a72548cab28fda732e38cf682106f92d564b25
yu.wang <yu.wang@viteusa.com>
2021-08-16 17:33:58 -0400

    viteusa fix

--- config/config-key.properties.js
+++ config/config-key.properties.js
@@ -2,35 +2,12 @@
 
 module.exports = {
   development: {
-    apiKey: 'AIzaSyAlYn1SqsfnhEB3izv5fgREYmXgnc7OnGw',
-    authDomain: 'easywarehouse-1610a.firebaseapp.com',
-    databaseURL: 'https://easywarehouse-1610a.firebaseio.com',
-    projectId: 'easywarehouse-1610a',
-    storageBucket: "easywarehouse-1610a.appspot.com",
-    messagingSenderId: "1073016836771"
-  },
-  core: {
-    apiKey: "AIzaSyB4m9EEN84YxYlYVbfH5gHQR_p4rEBsAgo",
-    authDomain: "easygo-core.firebaseapp.com",
-    databaseURL: "https://easygo-core.firebaseio.com",
-    projectId: "easygo-core",
-    storageBucket: "easygo-core.appspot.com",
-    messagingSenderId: "752259949354"
-  },
-  stage: {
-    apiKey: "AIzaSyCLIO74aOSy6OVSHb5EvwyvmH-HqTGlfwI",
-    authDomain: "easygo-warehouse.firebaseapp.com",
-    databaseURL: "https://easygo-warehouse.firebaseio.com",
-    projectId: "easygo-warehouse",
-    storageBucket: "easygo-warehouse.appspot.com",
-    messagingSenderId: "90347533693"
-  },
-  production: {
-    apiKey: "AIzaSyAWwmzzdtfd28nYep_8GXh602vfP3mmvOo",
-    authDomain: "viteusa-prod.firebaseapp.com",
-    databaseURL: "https://viteusa-prod.firebaseio.com",
-    projectId: "viteusa-prod",
-    storageBucket: "viteusa-prod.appspot.com",
-    messagingSenderId: "21890505355"
+    databaseURL: 'https://test-7c0df.firebaseapp.com',
+    apiKey: "AIzaSyCV2De2rigwYNPF6ndI6fE8RkhPX2IeP1U",
+    authDomain: "test-7c0df.firebaseapp.com",
+    projectId: "test-7c0df",
+    storageBucket: "test-7c0df.appspot.com",
+    messagingSenderId: "1067309532598",
+    appId: "1:1067309532598:web:c41a0c19adadef609ead17"
   }
 }
