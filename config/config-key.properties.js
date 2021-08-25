b9a5b4ed3974298dd9d430b1e2aa794f653f95a1
yu.wang <yu.wang@viteusa.com>
2021-08-25 16:06:38 -0400

    update key

--- config/config-key.properties.js
+++ config/config-key.properties.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   development: {
-    databaseURL: 'https://test-7c0df.firebaseapp.com',
-    apiKey: "AIzaSyCV2De2rigwYNPF6ndI6fE8RkhPX2IeP1U",
-    authDomain: "test-7c0df.firebaseapp.com",
-    projectId: "test-7c0df",
-    storageBucket: "test-7c0df.appspot.com",
-    messagingSenderId: "1067309532598",
-    appId: "1:1067309532598:web:c41a0c19adadef609ead17"
+    databaseURL: 'https://test123-6f5f9.firebaseapp.com',
+    apiKey: "AIzaSyBojgYtm7Juv9nmOHblKKr9TSSxWLHHgy8",
+    authDomain: "test123-6f5f9.firebaseapp.com",
+    projectId: "test123-6f5f9",
+    storageBucket: "test123-6f5f9.appspot.com",
+    messagingSenderId: "438308510614",
+    appId: "1:438308510614:web:102c45d9630802b5901bb7"
   }
 }
